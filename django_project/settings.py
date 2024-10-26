7897ce6c35f04cd8ad6cf48d2e1ce590bb35fc67
anandadagit <anand@ada.ac.uk>
2024-10-26 10:16:26 +0100

    sending email does not work

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +135,22 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 MEDIA_ROOT = BASE_DIR / 'media' # Newer modern and Pythonic way
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+print(EMAIL_BACKEND)
+print(EMAIL_HOST)
+print(EMAIL_PORT)
+print(EMAIL_USE_TLS)
+print("user" + EMAIL_HOST_USER)
+print(EMAIL_HOST_PASSWORD)
+print(DEFAULT_FROM_EMAIL)
+
+
+
