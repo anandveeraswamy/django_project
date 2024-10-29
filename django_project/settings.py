596f18a838d7021686cf4fe3b44a074148b6ec88
anandveeraswamy <anandveeraswamy@gmail.com>
2024-10-29 05:27:27 +0000

    changed getenv to environ.get

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-)k492et36_^e^mia%65zhaba^&lerd8jnp9b4)wwx(w_oh#7g^'
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG') == 'TRUE'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 #print(DEBUG)
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Login email address, Stored as an environment varaible
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # Login email address, Stored as an environment varaible
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
